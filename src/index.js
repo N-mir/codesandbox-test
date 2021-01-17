03fc831deea738bbfc4cb107cb7b9d224211621e
N-mir <47846195+N-mir@users.noreply.github.com>
2021-01-17 23:04:10 +0900

    入力内容を「未完了のTODO」に追加

--- src/index.js
+++ src/index.js
@@ -2,9 +2,30 @@ import "./styles.css";
 
 const onClickAdd = () => {
   // alert();
+
+  // テキストボックスの値を取得
   const inputText = document.getElementById("add-text").value;
+  // テキストボックスの値を初期化
   document.getElementById("add-text").value = "";
-  alert(inputText);
+  // alert(inputText);
+
+  // TODOリスト(divタグ部分)の生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  // console.log(div);
+
+  // liタグの生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  // console.log(li);
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  // console.log(div);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+  console.log(div);
 };
 
 document
